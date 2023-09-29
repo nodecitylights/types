aa6ef92736037718c92088b861f513f5dee0079b
Samantha <contact@samanthanguyen.me>
2023-09-29 01:36:53 -0500

    docs: add documentation to all type exports (#9)

--- src/logic.d.ts
+++ src/logic.d.ts
@@ -1,7 +1,46 @@
+/**
+ * A type that represents the NOT logic gate.
+ *
+ * The NOT logic gate has the following truth table:
+ *
+ * | A | NOT A |
+ * |---|-------|
+ * | 0 |   1   |
+ * | 1 |   0   |
+ *
+ * @template A The boolean to negate
+ * @example
+ * ```ts
+ * type Test1 = Not<true> // false
+ * type Test2 = Not<false> // true
+ * ```
+ */
 export type Not<A extends boolean> = A extends true
 	? false
 	: true
 
+/**
+ * A type that represents the AND logic gate.
+ *
+ * The AND logic gate has the following truth table:
+ *
+ * | A | B | A AND B |
+ * |---|---|---------|
+ * | 0 | 0 |    0    |
+ * | 0 | 1 |    0    |
+ * | 1 | 0 |    0    |
+ * | 1 | 1 |    1    |
+ *
+ * @template A Boolean input A
+ * @template B Boolean input B
+ * @example
+ * ```ts
+ * type Test1 = And<true, true>   // true
+ * type Test2 = And<true, false>  // false
+ * type Test3 = And<false, true>  // false
+ * type Test4 = And<false, false> // false
+ * ```
+ */
 export type And<A extends boolean, B extends boolean> =
 	A extends true
 		? ( B extends true
@@ -9,19 +48,130 @@ export type And<A extends boolean, B extends boolean> =
 			: false
 	) : false
 
+/**
+ * A type that represents the NAND logic gate.
+ *
+ * The NAND logic gate has the following truth table:
+ * 
+ * | A | B | A NAND B |
+ * |---|---|----------|
+ * | 0 | 0 |     1    |
+ * | 0 | 1 |     1    |
+ * | 1 | 0 |     1    |
+ * | 1 | 1 |     0    |
+ *
+ * @template A Boolean input A
+ * @template B Boolean input B
+ * @example
+ * ```ts
+ * type Test1 = Nand<true, true>   // false
+ * type Test2 = Nand<true, false>  // true
+ * type Test3 = Nand<false, true>  // true
+ * type Test4 = Nand<false, false> // true
+ * ```
+ */
 export type Nand<A extends boolean, B extends boolean> = Not<And<A, B>>
 
+/**
+ * A type that represents the XOR logic gate.
+ *
+ * The XOR logic gate has the following truth table:
+ * 
+ * | A | B | A XOR B |
+ * |---|---|---------|
+ * | 0 | 0 |    0    |
+ * | 0 | 1 |    1    |
+ * | 1 | 0 |    1    |
+ * | 1 | 1 |    0    |
+ *
+ * @template A Boolean input A
+ * @template B Boolean input B
+ * @example
+ * ```ts
+ * type Test4 = Xor<false, false> // false
+ * type Test3 = Xor<false, true>  // true
+ * type Test2 = Xor<true, false>  // true
+ * type Test1 = Xor<true, true>   // false
+ * ```
+ */
 export type Xor<A extends boolean, B extends boolean> = 
 	Nand<
 		Nand<A, Nand<A, B>>,
 		Nand<B, Nand<A, B>>
 	>
 
+/**
+ * A type that represents the XNOR logic gate.
+ * 
+ * The XNOR logic gate has the following truth table:
+ * 
+ * | A | B | A XNOR B |
+ * |---|---|----------|
+ * | 0 | 0 |     1    |
+ * | 0 | 1 |     0    |
+ * | 1 | 0 |     0    |
+ * | 1 | 1 |     1    |
+ *
+ * @template A Boolean input A
+ * @template B Boolean input B
+ * @example
+ * ```ts
+ * type Test1 = Xnor<true, true>   // true
+ * type Test3 = Xnor<false, true>  // false
+ * type Test2 = Xnor<true, false>  // false
+ * type Test4 = Xnor<false, false> // true
+ * ```
+ */
 export type Xnor<A extends boolean, B extends boolean> =
 	Nand<
 		Nand<Nand<A, A>, Nand<B, B>>,
 		Nand<A, B>
 	>
 
+/**
+ * A type that represents the OR logic gate.
+ *
+ * The OR logic gate has the following truth table:
+ *
+ * | A | B | A OR B |
+ * |---|---|--------|
+ * | 0 | 0 |   0    |
+ * | 0 | 1 |   1    |
+ * | 1 | 0 |   1    |
+ * | 1 | 1 |   1    |
+ *
+ * @template A Boolean input A
+ * @template B Boolean input B
+ * @example
+ * ```ts
+ * type Test4 = Or<false, false> // false
+ * type Test3 = Or<false, true>  // true
+ * type Test2 = Or<true, false>  // true
+ * type Test1 = Or<true, true>   // true
+ * ```
+ */
 export type Or<A extends boolean, B extends boolean> = Nand<Not<A>, Not<B>>
+
+/**
+ * A type that represents the NOR logic gate.
+ *
+ * The NOR logic gate has the following truth table:
+ * 
+ * | A | B | A NOR B |
+ * |---|---|---------|
+ * | 0 | 0 |    1    |
+ * | 0 | 1 |    0    |
+ * | 1 | 0 |    0    |
+ * | 1 | 1 |    0    |
+ *
+ * @template A Boolean input A
+ * @template B Boolean input B
+ * @example
+ * ```ts
+ * type Test4 = Nor<false, false> // true
+ * type Test3 = Nor<false, true>  // false
+ * type Test2 = Nor<true, false>  // false
+ * type Test1 = Nor<true, true>   // false
+ * ```
+ */
 export type Nor<A extends boolean, B extends boolean> = Not<Or<A, B>>
--- src/strings.d.ts
+++ src/strings.d.ts
@@ -1,16 +1,140 @@
+/**
+ * Utility union type for use with generic types like
+ * `AffixedWith` and `RemoveAffix`
+ *
+ * @example
+ * ```ts
+ * type Test1 = AffixedWith<'Hello', 'H', 'prefix'> // true
+ * type Test2 = AffixedWith<'Hello', 'H', 'suffix'> // false
+ * 
+ * type Test3 = RemoveAffix<'HelloWorld', 'Hello', 'prefix'> // 'World'
+ * type Test4 = RemoveAffix<'HelloWorld', 'World', 'suffix'> // 'Hello'
+ * ```
+ */
 export type Affix = 'suffix' | 'prefix'
 
+/**
+ * A type that indicates whether a string starts with a given prefix,
+ * and evaluates to a boolean
+ *
+ * @template V The string value to check
+ * @template P The prefix to check for
+ * @example
+ * ```ts
+ * type Test1 = StartsWith<'Hello', 'H'> // true
+ * type Test2 = StartsWith<'Hello', 'e'> // false
+ * ```
+ */
 export type StartsWith<V extends string, P extends string> = V extends `${P}${string}` ? true : false
+
+/**
+ * A type that indicates whether a string ends with a given suffix,
+ * and evaluates to a boolean
+ *
+ * @template V The string value to check
+ * @template S The suffix to check for
+ * @example
+ * ```ts
+ * type Test1 = EndsWith<'Hello', 'o'> // true
+ * type Test2 = EndsWith<'Hello', 'H'> // false
+ * ```
+ */
 export type EndsWith<V extends string, S extends string> = V extends `${string}${S}` ? true : false
-export type AffixedWith<V extends string, Lit extends string, A extends Affix> = 
+
+/**
+ * A type that indicates whether a string starts with a given prefix or suffix,
+ * and evaluates to a boolean
+ *
+ * @template V The string value to check
+ * @template Lit The prefix or suffix to check for
+ * @template A The kind of affix to check for (`'prefix'` or `'suffix'`)
+ * @example
+ * ```ts
+ * type Test1 = AffixedWith<'Hello', 'H', 'prefix'> // true
+ * type Test2 = AffixedWith<'Hello', 'e', 'suffix'> // false
+ * ```
+ */
+export type AffixedWith<V extends string, Lit extends string, A extends Affix> =
   A extends 'suffix' ? EndsWith<V, Lit> :
   A extends 'prefix' ? StartsWith<V, Lit> : never
 
+/**
+ * A type that creates a new string literal given
+ * a prefix of `P` and string value of `V`
+ *
+ * @template V The string value to prefix
+ * @template P The prefix to prepend
+ * @example
+ * ```ts
+ * type Test1 = PrefixWith<'Alice!', 'Hi'> // 'Hi Alice!'
+ * type Test2 = PrefixWith<'Bob!', 'Hi'> // 'Hi Bob!'
+ * ```
+ */
 export type PrefixWith<V extends string, P extends string> = `${P}${V}`
+
+/**
+ * A type that creates a new string literal given
+ * a suffix of `S` and string value of `V`
+ * 
+ * @template V The string value to suffix
+ * @template S The suffix to append to
+ * @example
+ * ```ts
+ * type Test1 = SuffixWith<'Hi', '!'> // 'Hi!'
+ * ```
+ */
 export type SuffixWith<V extends string, S extends string> = `${V}${S}`
 
+/**
+ * A type that creates a new string literal given
+ * a string value of `V` and a prefix of `Lit`.
+ *
+ * If `V` does not start with `Lit`, then `V` is returned.
+ *
+ * @template V The string value to remove the prefix from
+ * @template Lit The prefix literal to remove
+ * @example
+ * ```ts
+ * type Test1 = RemovePrefix<'HelloWorld', 'Hello'> // 'World'
+ * type Test2 = RemovePrefix<'HelloWorld', 'World'> // 'HelloWorld'
+ * ```
+ */
 export type RemovePrefix<V extends string, Lit extends string> = V extends `${Lit}${infer P}` ? P : V;
+
+/**
+ * A type that creates a new string literal given
+ * a string value of `V` and a suffix of `Lit`.
+ * 
+ * If `V` does not end with `Lit`, then `V` is returned.
+ * 
+ * @template V The string value to remove the suffix from
+ * @template Lit The suffix literal to remove
+ * @example
+ * ```ts
+ * type Test1 = RemoveSuffix<'HelloWorld', 'World'> // 'Hello'
+ * type Test2 = RemoveSuffix<'HelloWorld', 'Hello'> // 'HelloWorld'
+ * ```
+ */
 export type RemoveSuffix<V extends string, Lit extends string> = V extends `${infer P}${Lit}` ? P : V;
+
+/**
+ * A type that creates a new string literal given
+ * a string value of `V`, a literal of `Lit`,
+ * and an affix kind of `A`
+ * 
+ * - If `A` is `'prefix'`, then `Lit` is removed from the start of `V`.
+ * - If `A` is `'suffix'`, then `Lit` is removed from the end of `V`.
+ * - If `V` does not end/start with `Lit`, then `V` is returned.
+ * 
+ * @template V The string value to remove the affix from
+ * @template Lit The affix literal to remove
+ * @template A The kind of affix to remove (`'prefix'` or `'suffix'`)
+ * @example
+ * ```ts
+ * type Test1 = RemoveAffix<'HelloWorld', 'Hello', 'prefix'> // 'World'
+ * type Test2 = RemoveAffix<'HelloWorld', 'World', 'suffix'> // 'Hello'
+ * ```
+ */
 export type RemoveAffix<V extends string, Lit extends string, A extends Affix> =
   A extends 'suffix' ? RemoveSuffix<V, Lit> :
   A extends 'prefix' ? RemovePrefix<V, Lit> : never
