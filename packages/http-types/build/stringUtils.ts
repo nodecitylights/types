06a682649c471647545f5a4768f2947014f8bf47
Samantha <contact@samanthanguyen.me>
2023-12-21 01:50:25 -0600

    perf: speed up http-types build script with binary search (#42)

--- packages/http-types/build/stringUtils.ts
+++ packages/http-types/build/stringUtils.ts
@@ -22,29 +22,51 @@ export function getHttpMethodAsCamelCase(method: string): string {
 	}
 }
 
+const ForbiddenHttpRequestHeaders = [
+	'Accept-Charset',
+	'Accept-Encoding',
+	'Access-Control-Request-Headers',
+	'Access-Control-Request-Method',
+	'Connection',
+	'Content-Length',
+	'Cookie',
+	'DNT',
+	'Date',
+	'Expect',
+	'Feature-Policy',
+	'Host',
+	'Keep-Alive',
+	'Origin',
+	'Referer',
+	'TE',
+	'Trailer',
+	'Transfer-Encoding',
+	'Upgrade',
+	'Via',
+];
+
 export function isForbiddenHttpRequestHeader(header: string): boolean {
 	return header.startsWith('Proxy')
 		|| header.startsWith('Sec')
-		|| [
-			'Accept-Charset',
-			'Accept-Encoding',
-			'Access-Control-Request-Headers',
-			'Access-Control-Request-Method',
-			'Connection',
-			'Content-Length',
-			'Cookie',
-			'Date',
-			'DNT',
-			'Expect',
-			'Feature-Policy',
-			'Host',
-			'Keep-Alive',
-			'Origin',
-			'Referer',
-			'TE',
-			'Trailer',
-			'Transfer-Encoding',
-			'Upgrade',
-			'Via',
-		].includes(header);
+		|| binarySearch(ForbiddenHttpRequestHeaders, header);
+}
+
+function binarySearch(array: string[], value: string): boolean {
+	let left = 0;
+	let right = array.length - 1;
+
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2);
+		const midValue = array[mid];
+	
+		if (midValue === value) {
+			return true; // Found the target string
+		} else if (midValue < value) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+
+	return false;
 }
