1a0731c9a7ae319608fd314482cf9085ea8e3b52
Samantha Nguyen <contact@samanthanguyen.me>
2023-12-23 20:50:24 -0600

    refactor: move makeFullDocBlock() out of main build script

--- packages/http-types/build/docUtils.ts
+++ packages/http-types/build/docUtils.ts
@@ -1,3 +1,13 @@
+import wordWrap from 'word-wrap';
+
+import type { ConceptValue } from './conceptTypes';
+import {
+	IetfDataTrackerLabelProvider,
+	UrlLabelProvider,
+	W3OrgLabelProvider,
+	WicgLabelProvider
+} from './UrlLabelProvider';
+
 export function makeDocTag(annotation: string, value: string): string {
 	return `@${annotation} ${value}`;
 }
@@ -12,5 +22,38 @@ export function makeDocBlock(values: string[]): string {
 		docBlock += ` *${value === '' ? '\n' : ` ${value}\n`}`;
 	}
 	docBlock += ' */';
+	return docBlock;
+}
+
+const labelProvider = new UrlLabelProvider([
+	new W3OrgLabelProvider(),
+	new WicgLabelProvider(),
+	new IetfDataTrackerLabelProvider(),
+]);
+
+export function makeFullDocBlock(conceptValue: ConceptValue): string {
+	const description = wordWrap(conceptValue.details[0].description, { width: 60, indent: '' });
+	const lines = description.split('\n');
+
+	const docLinks: string[] = [];
+	const specLinks: string[] = [];
+
+	for(const detail of conceptValue.details) {
+		docLinks.push(makeDocSeeTag(
+			`Documentation → ${labelProvider.provideLabel(detail, true)}`,
+			new URL(detail.documentation)));
+
+		specLinks.push(makeDocSeeTag(
+			`Specification → ${labelProvider.provideLabel(detail, false)}`,
+			new URL(detail.specification)));
+	}
+
+	const docBlock = makeDocBlock([
+		...lines.map((line) => line.trim()),
+		'',
+		...docLinks,
+		...specLinks,
+	]);
+
 	return docBlock;
 }
